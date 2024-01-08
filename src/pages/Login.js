43048564c0dec9344a9c00ce281bf3d837003f84
nicholasr74 <nicholasramroop74@gmail.com>
2024-01-08 14:08:40 -0400

    started work on login component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,1 +1,33 @@
-import React from 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+function Login(){
+
+    return(
+        <>
+            <div className = "bg-white rounded-lg drop-shadow-xl p-8">
+                <form className = "flex flex-col items-start">
+                    <label className = "flex flex-col items-start">
+                        Username
+                    <input
+                    type = "text"
+                    placeholder='Username'
+                    ></input>
+                    </label>
+                    <label>
+                        Password
+                        <input></input>
+                    </label>
+                </form>
+
+
+
+            </div>
+        </>
+    )
+}
+
+
+export default Login;
