48f816dcdcf71c95731c0d6aa9fd07af76c8046a
nicholasr74 <nicholasramroop74@gmail.com>
2024-01-08 21:29:15 -0400

    minor polishing

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -11,7 +11,7 @@ function SignupPage(){
             <div className = "flex flex-1 p-5 bg-blue-400 items-center justify-center text-white drop-shadow-md">
                 <p className = "flex flex-1 justify-center text-4xl">Sign Up</p>    
                 
-                <NavLink to = "/login" as = "button" className = "flex flex-10 bg-blue  rounded text-l">
+                <NavLink to = "/" as = "button" className = "flex flex-10 bg-blue  rounded text-l">
                    Log In
                 </NavLink> 
                 </div>
